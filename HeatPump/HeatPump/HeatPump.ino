ed57adfbc1999edf0efb99311a17c4282bd9a284
Igor Shevchenko <igorgsh@gmail.com>
2017-05-14 11:36:23 +0300

    Initial

---
+++ HeatPump/HeatPump/HeatPump.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		HeatPump.ino
+ Created:	14.05.17 11:26:52
+ Author:	Igor Shevchenko
+
+ Heat Pump controller
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
